0c0b00b7b663ef5892fb1a0d7814e8c6e9545016
OussamaTh <oussamath84@gmail.com>
2025-12-30 09:06:52 +0000

    d

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/mpdv",
+  base: "/",
   server: {
     host: '0.0.0.0',
     strictPort: true
