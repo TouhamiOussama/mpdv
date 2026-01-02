84ce4daef07ffa60c2d6bea703d4ee0b1790f041
OussamaTh <oussamath84@gmail.com>
2026-01-02 19:43:29 +0000

    d

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/",
+  base: "/oussama",
   server: {
     host: '0.0.0.0',
     strictPort: true
