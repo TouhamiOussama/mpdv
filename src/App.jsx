660c131e2f6460aac049eab581a196fd5bd7abb6
OussamaTh <oussamath84@gmail.com>
2026-01-12 13:18:59 +0000

    d

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,10 @@ function App() {
 
   return (
     <>
-      {/* <Portfolio /> */}
-      <div className="min-h-[100vh] flex items-center justify-center ">
+      <Portfolio />
+     {/*  <div className="min-h-[100vh] flex items-center justify-center ">
         <h1 className="darkBorderedText text-[4rem] max-md:text-[3rem] text-center font-[900]" >Not available at the moment !</h1>
-      </div>
+      </div> */}
     </>
   )
 }
